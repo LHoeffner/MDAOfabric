a01897ba2a88586254a4235d6f3ab06a2c5675d5
Lukas Hoeffner <lukas.hoeffner@tum.de>
2019-05-27 14:43:28 +0200

    added GenericIteratingBundleSolver

--- MDAOfabric/solvers/__init__.py
+++ MDAOfabric/solvers/__init__.py
@@ -5,3 +5,4 @@ from .iterating_bundle_solver_base import IteratingBundleSolverBase
 
 # implementations
 from .empty_solver import EmptySolver
+from .generic_iterating_bundle_solver import GenericIteratingBundleSolver
---
+++ MDAOfabric/solvers/generic_iterating_bundle_solver.py
@@ -0,0 +1,26 @@
+import MDAOfabric
+from .iterating_bundle_solver_base import IteratingBundleSolverBase
+
+
+class GenericIteratingBundleSolver(IteratingBundleSolverBase):
+    """Solver class for iterations containing multiple solvers which are chosen via the settings file
+
+    """
+    def __init__(self, settings):
+        super(GenericIteratingBundleSolver, self).__init__(settings)
+
+        # create stack of solvers and IO
+        for piece_name, piece_settings in self.settings['stack'].items():
+            if 'name' not in piece_settings:
+                piece_settings['name'] = piece_name
+
+            piece = MDAOfabric.factory.GetPiece(piece_settings['type'])
+            self.stack.append(piece)
+
+    def GetDefaultSettings(self):
+        defaults = MDAOfabric.Settings('''{
+            "name"          : "GenericIteratingBundleSolver",
+            "no_iterations" : 5
+        }''')
+        defaults['stack'] = MDAOfabric.Settings('')
+        return defaults
